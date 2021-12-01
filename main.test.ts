d85ebf8344aa388605af3e525467bc098534d98e
Arik Maor <arik.maor@tensorleap.ai>
2021-12-01 14:37:16 +0200

    Fix snapshot tests infra

--- main.test.ts
+++ main.test.ts
@@ -1,11 +1,9 @@
 import { MyChart } from './main';
 import { Testing } from 'cdk8s';
 
-describe('Placeholder', () => {
-  test('Empty', () => {
-    const app = Testing.app();
-    const chart = new MyChart(app, 'test-chart');
-    const results = Testing.synth(chart);
-    expect(results).toMatchSnapshot();
-  });
+test('On-prem manifests', () => {
+  const app = Testing.app();
+  new MyChart(app, 'test-chart');
+  const results = app.synthYaml();
+  expect(results).toMatchSnapshot();
 });
