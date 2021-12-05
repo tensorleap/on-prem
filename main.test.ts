2b548e891c960d4faab2041028dd63255f526783
Arik Maor <arik.maor@tensorleap.ai>
2021-12-05 21:53:25 +0200

    Update tests and snapshots

--- main.test.ts
+++ main.test.ts
@@ -1,11 +1,27 @@
-import { WebUi } from './constructs/web-ui';
 import { Testing } from 'cdk8s';
+import { WebUi } from './constructs/web-ui';
+import { Elasticsearch } from './constructs/elasticsearch';
+import { Kibana } from './constructs/kibana';
 
 test('Web UI', () => {
   const app = Testing.app();
   new WebUi(app, {
     imageTag: 'master-1234568-stable',
   });
+  const results = app.synthYaml();
+  expect(results).toMatchSnapshot();
+});
+
+test('Elasticsearch', () => {
+  const app = Testing.app();
+  new Elasticsearch(app);
+  const results = app.synthYaml();
+  expect(results).toMatchSnapshot();
+});
+
+test('Kibana', () => {
+  const app = Testing.app();
+  new Kibana(app);
   const results = app.synthYaml();
   expect(results).toMatchSnapshot();
 });
