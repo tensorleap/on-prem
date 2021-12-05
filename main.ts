c44a4ccc37ae98ab1e9bedb448c421d5aaf4cdbf
Arik Maor <arik.maor@tensorleap.ai>
2021-12-05 21:50:05 +0200

    Add elasticsearch

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { App } from 'cdk8s';
 import { WebUi } from './constructs/web-ui';
+import { Elasticsearch } from './constructs/elasticsearch';
 
 const app = new App();
 new WebUi(app, {
   imageTag: 'run-on-prem-77547f29-stable',
 });
+new Elasticsearch(app);
 app.synth();
