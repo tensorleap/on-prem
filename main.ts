5f1326c59a9276816d40c49db07a8b5579b4bb33
Arik Maor <arik.maor@tensorleap.ai>
2021-12-05 21:53:07 +0200

    Add kibanimat

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { App } from 'cdk8s';
 import { WebUi } from './constructs/web-ui';
 import { Elasticsearch } from './constructs/elasticsearch';
+import { Kibana } from './constructs/kibana';
 
 const app = new App();
 new WebUi(app, {
   imageTag: 'run-on-prem-77547f29-stable',
 });
 new Elasticsearch(app);
+new Kibana(app);
 app.synth();
