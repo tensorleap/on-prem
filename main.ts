a117e481219309c148e0dabc186a19a2f157848e
Arik Maor <arik.maor@tensorleap.ai>
2021-12-08 15:47:32 +0200

    Add node-server

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
 import { App } from 'cdk8s';
 import { WebUi } from './constructs/web-ui';
+import { NodeServer } from './constructs/node-server';
 import { Elasticsearch } from './constructs/elasticsearch';
 import { Kibana } from './constructs/kibana';
 
 const app = new App();
+new Elasticsearch(app);
+new Kibana(app);
+new NodeServer(app, {
+  imageTag: 'master-8b98feef-stable',
+});
 new WebUi(app, {
   imageTag: 'master-36b486b3-stable',
 });
-new Elasticsearch(app);
-new Kibana(app);
 app.synth();
