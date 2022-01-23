87d7cd11f607053c394a3cfdf09d361c87988434
Arik Maor <arik.maor@tensorleap.ai>
2022-01-23 20:27:11 +0200

    Create zero-replicas index template after elasticsearch starts
    This will set `number_of_replicas` to `0` for every newly created index

--- constructs/elasticsearch.ts
+++ constructs/elasticsearch.ts
@@ -29,6 +29,31 @@ export class Elasticsearch extends Chart {
             value: '',
           },
         ],
+        lifecycle: {
+          postStart: {
+            exec: {
+              command: [
+                'bash',
+                '-c',
+
+                `#!/bin/bash
+set -euo pipefail;
+while [[ "$(curl -s -o /dev/null -w '%{http_code}\n' -L 127.0.0.1:9200/_cluster/health?wait_for_status=yellow&timeout=5s)" != "200" ]]; do sleep 3; done
+
+echo 'Setting zero-replicas index template';
+curl 127.0.0.1:9200/_template/zero-replicas -s --fail -XPUT -H 'Content-Type: application/json' -d '${JSON.stringify(
+                  {
+                    index_patterns: ['*'],
+                    settings: {
+                      number_of_replicas: 0,
+                    },
+                  }
+                )}'
+`,
+              ],
+            },
+          },
+        },
         antiAffinity: 'soft',
         esJavaOpts: '-Xmx128m -Xms128m -Dlog4j2.formatMsgNoLookups=true',
         resources: {
