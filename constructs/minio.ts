301c2f79f73d0efd04d796f9aee6b0607b3e1f92
yotamazriel <yotam@tensorleap.ai>
2022-03-13 16:56:40 +0200

    Increase proxy-body-size

--- constructs/minio.ts
+++ constructs/minio.ts
@@ -50,7 +50,7 @@ export class Minio extends Chart {
     new KubeIngress(this, 'ingress', {
       metadata: {
         annotations: {
-          'nginx.ingress.kubernetes.io/proxy-body-size': '100m',
+          'nginx.ingress.kubernetes.io/proxy-body-size': '900m',
           'kubernetes.io/ingress.class': 'public',
           'nginx.ingress.kubernetes.io/upstream-vhost': `${minio.releaseName}:9000`,
         },
